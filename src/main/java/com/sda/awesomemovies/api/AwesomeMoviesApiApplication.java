4b6ddfcc5e3dce329eaf36d920437f4e752531fe
Tomek Owczarek <violeticus@gmail.com>
2018-09-09 13:34:00 +0200

    SPring init template generated
    Add postgres conection
    
    Disable flyway
    
    Trying flyway + test table
    
    Update flyway version
    
    Add plugin to flyway
    
    Try with additional envs
    
    Try database only without flyway
    
    Add configuration to postgres db
    
    Try different connection pool
    
    Try with limit param
    
    Change pool size to 5
    
    Switch to elephant service DB, make flyway work

---
+++ src/main/java/com/sda/awesomemovies/api/AwesomeMoviesApiApplication.java
@@ -0,0 +1,12 @@
+package com.sda.awesomemovies.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeMoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeMoviesApiApplication.class, args);
+	}
+}
