0cd7425342dd35722de506016698f979a81aaad1
Tomek Owczarek <violeticus@gmail.com>
2018-09-16 13:50:10 +0200

    Hotfix tests

--- src/test/java/com/sda/awesomemovies/api/AwesomeMoviesApiApplicationTests.java
+++ src/test/java/com/sda/awesomemovies/api/AwesomeMoviesApiApplicationTests.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/test/java/com/sda/awesomemovies/api/AwesomeMoviesApiApplicationTests.java
 package com.sda.awesomemovies.api;
 
 import org.junit.Test;
@@ -13,23 +12,4 @@ public class AwesomeMoviesApiApplicationTests {
     @Test
     public void contextLoads() {
     }
-
 }
-=======
-package com.sda.awesomemovies.api;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class AwesomeMoviesApiApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
-}
->>>>>>> d58e6147c1be66ec2708fbf9902d0bd54c673c67:src/test/java/com/sda/awesomemovies/api/AwesomeMoviesApiApplicationTests.java
