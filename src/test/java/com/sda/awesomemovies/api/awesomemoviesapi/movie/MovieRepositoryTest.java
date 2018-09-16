f260b2f8910bfed202ab4f4d35259b8f05dc9c73
Piotr <pszczesny087@gmail.com>
2018-09-16 11:03:08 +0200

    fixed test

--- src/test/java/com/sda/awesomemovies/api/awesomemoviesapi/movie/MovieRepositoryTest.java
+++ src/test/java/com/sda/awesomemovies/api/awesomemoviesapi/movie/MovieRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,6 +18,12 @@ public class MovieRepositoryTest {
     @Test
     public void addingNewMovies() {
         movieRepository.save(new MovieEntity("Matrix"));
+        MovieEntity matrix = movieRepository.findByTitle("Matrix");
+        String titleFromSql = matrix.getTitle();
+
+        assertSame("Matrix", titleFromSql);
     }
+
+
 
 }
