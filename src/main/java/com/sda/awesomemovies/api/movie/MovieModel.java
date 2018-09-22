f2e9862aac8e204ae988c8629e1cfcd5bd593a0a
Krzysztof Songowicz <krzysztof.songowicz@outlook.com>
2018-09-22 11:00:48 +0200

    Create MovieAPI - MovieModel - MovieService - MovieController refactoring

---
+++ src/main/java/com/sda/awesomemovies/api/movie/MovieModel.java
@@ -0,0 +1,16 @@
+package com.sda.awesomemovies.api.movie;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieModel {
+    private Integer id;
+    private String title;
+
+}
