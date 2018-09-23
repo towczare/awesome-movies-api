d9a06ccdbe3bce5d42e78de7823b5e6a98d8855e
Krzysztof Songowicz <krzysztof.songowicz@outlook.com>
2018-09-23 10:42:53 +0200

    Quick fix - errors in SQL statements

--- src/main/resources/db/migration/V9__add_movie_data.sql
+++ src/main/resources/db/migration/V9__add_movie_data.sql
@@ -4,32 +4,32 @@ ALTER TABLE movie
 INSERT INTO movie
 VALUES (2, 'Stranger Things', 'Matt Duffer');
 INSERT INTO movie_category
-VALUES (2, (SELECT category_id FROM category WHERE name == 'horror'));
+VALUES (2, (SELECT category_id FROM category WHERE name = 'horror'));
 INSERT INTO movie_category
-VALUES (2, (SELECT category_id FROM category WHERE name == 'thriller'));
+VALUES (2, (SELECT category_id FROM category WHERE name = 'thriller'));
 INSERT INTO movie_category
-VALUES (2, (SELECT category_id FROM category WHERE name == 'SCI-FI'));
+VALUES (2, (SELECT category_id FROM category WHERE name = 'SCI-FI'));
 
 INSERT INTO movie
 VALUES (3, 'Orange is The New Black', 'Jenji Kohan');
 INSERT INTO movie_category
-VALUES (3, (SELECT category_id FROM category WHERE name == 'komedia'));
+VALUES (3, (SELECT category_id FROM category WHERE name = 'komedia'));
 INSERT INTO movie_category
-VALUES (3, (SELECT category_id FROM category WHERE name == 'dramat'));
+VALUES (3, (SELECT category_id FROM category WHERE name = 'dramat'));
 
 INSERT INTO movie
 VALUES (4, 'Dark', 'Baran bo Odar');
 INSERT INTO movie_category
-VALUES (4, (SELECT category_id FROM category WHERE name == 'thriller'));
+VALUES (4, (SELECT category_id FROM category WHERE name = 'thriller'));
 
 INSERT INTO movie
 VALUES (5, 'Friends', 'David Crane');
 INSERT INTO movie_category
-VALUES (5, (SELECT category_id FROM category WHERE name == 'komedia'));
+VALUES (5, (SELECT category_id FROM category WHERE name = 'komedia'));
 
 INSERT INTO movie
 VALUES (6, 'Belfer', 'Bartłomiej Ignaciuk');
 INSERT INTO movie_category
-VALUES (6, (SELECT category_id FROM category WHERE name == 'thriller'));
+VALUES (6, (SELECT category_id FROM category WHERE name = 'thriller'));
 INSERT INTO movie_category
-VALUES (6, (SELECT category_id FROM category WHERE name == 'dramat'));
+VALUES (6, (SELECT category_id FROM category WHERE name = 'dramat'));
