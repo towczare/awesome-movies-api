e94d7c08ad5ddd6265cff057a5cd534d1a1d1fd3
Rafał Tarnawski <39613222+rtarnawski@users.noreply.github.com>
2018-12-15 11:25:40 +0100

    18 movie trailer (#27)
    #18 Add trailer url (youtube service) in MovieEntity and all layers of api component to be visible in details view.

---
+++ src/main/resources/db/migration/V24__new_trailers_urls.sql
@@ -0,0 +1,23 @@
+UPDATE movie
+SET trailer_url = 'https://www.youtube.com/embed/s7EdQ4FqbhY'
+WHERE title = 'Pulp Fiction';
+
+UPDATE movie
+SET trailer_url = 'https://www.youtube.com/embed/AZwsKVL6JfM'
+WHERE title = 'Fight Club';
+
+UPDATE movie
+SET trailer_url = 'https://www.youtube.com/embed/jvcex-loSJ4'
+WHERE title = 'Forrest Gump';
+
+UPDATE movie
+SET trailer_url = 'https://www.youtube.com/embed/2ilzidi_J8Q'
+WHERE title = 'GoodFellas';
+
+UPDATE movie
+SET trailer_url = 'https://www.youtube.com/embed/AOIi9SjJvgU'
+WHERE title = 'The Lord of the Rings: The Return of the King';
+
+UPDATE movie
+SET trailer_url = 'https://www.youtube.com/embed/8BfMivMDOBI'
+WHERE title = 'Inception';
