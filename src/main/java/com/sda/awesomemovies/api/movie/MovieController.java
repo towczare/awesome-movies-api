67670abca1b6c0fe354d1e9338099b13f0706e88
rtarnawski <rtarnawski@gmail.com>
2018-12-15 13:01:54 +0100

    RequestMapping overload for paging

--- src/main/java/com/sda/awesomemovies/api/movie/MovieController.java
+++ src/main/java/com/sda/awesomemovies/api/movie/MovieController.java
@@ -2,6 +2,7 @@ package com.sda.awesomemovies.api.movie;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class MovieController {
     }
 
 
-    @RequestMapping("/moviespage")
+    @RequestMapping(value = "/movies", params = "page")
     public Page<MovieListModel> showMoviesPage(Pageable pageable) {
         return movieService.getAllMoviesPage(pageable);
     }
