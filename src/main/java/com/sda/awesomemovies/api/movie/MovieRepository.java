1679a2102c440ec18d820e0d349d55cddfe3983f
kat_kuczek <kat.kuczek.90@gmail.com>
2019-01-12 15:18:24 +0100

    Merge branch 'master' into kasia
    # Conflicts:
    #	src/main/java/com/sda/awesomemovies/api/movie/MovieController.java

--- src/main/java/com/sda/awesomemovies/api/movie/MovieRepository.java
+++ src/main/java/com/sda/awesomemovies/api/movie/MovieRepository.java
@@ -3,12 +3,11 @@ package com.sda.awesomemovies.api.movie;
 import com.querydsl.core.types.Predicate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MovieRepository extends PagingAndSortingRepository<MovieEntity, Integer>, QueryDslPredicateExecutor<MovieEntity> {
     MovieEntity findByTitle(String title);
